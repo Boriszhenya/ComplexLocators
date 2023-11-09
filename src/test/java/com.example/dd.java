417bb7846e5a21197204759be53501da03b9fd2d
Borys Pedorenko <boriszhenya@gmail.com>
2023-11-09 19:00:46 +0100

    HomeWork

--- src/test/java/com.example/dd.java
+++ src/test/java/com.example/dd.java
@@ -10,14 +10,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
-
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class LocatorsTest {
+public class dd {
 
     private WebDriver driver;
 
@@ -27,9 +26,9 @@ public class LocatorsTest {
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
-        Map<String, Object> prefs = new HashMap<>();
-        Map<String, Object> profile = new HashMap<>();
-        Map<String, Object> contentSettings = new HashMap<>();
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        Map<String, Object> profile = new HashMap<String, Object>();
+        Map<String, Object> contentSettings = new HashMap<String, Object>();
 
         //contentSettings.put("cookies",2);
         profile.put("managed_default_content_settings",contentSettings);
@@ -42,7 +41,7 @@ public class LocatorsTest {
         String baseUrl = "https://selectorshub.com/xpath-practice-page/";
         // Открываем главную страницу демо-сайта
         driver.get(baseUrl);
-        // Открываем бразуер на полный экран
+        // Открываем браузер на полный экран
         driver.manage().window().maximize();
 
         // Ждём пока страница появится - этот способ подходит только для демонстрации
@@ -56,28 +55,24 @@ public class LocatorsTest {
     @Test
     public void testComplexLocators() {
         //Выберите дату начала наших курсов в календаре, чтобы проверка прошла
-        WebElement dataInput = driver.findElement(By.xpath("//input[@type='date']"));
-        dataInput.sendKeys("06062023");
-
+        WebElement date = driver.findElement(By.xpath("/html/body/div[1]/main/div/div[1]/section[2]/div/div[2]/div/div/div/input[2]"));
+        date.sendKeys("06.06.2023");
+        //Проверка
         assertEquals("2023-06-06", driver.findElement(By.xpath("/html/body/div[1]/main/div/div[1]/section[2]/div/div[2]/div/div/div/input[2]")).getAttribute("value"));
 
         //После этого переходим к таблице
         WebElement elementH2 = driver.findElement(By.xpath("//h2[.='System Distribution Details']"));
         new Actions(driver).scrollToElement(elementH2).perform();
 
-        WebElement showEntries = driver.findElement(By.xpath("//select[@name='tablepress-1_length']"));
-        showEntries.sendKeys("25");
-
-        //WebElement checkBox = driver.findElement(By.xpath("/html/body/div[1]/main/div/div[1]/section[3]/div/div/div/div[3]/div/div/div/table/tbody/tr[22]/td[1]/input"));
-
-        //с этим локатором не хочет работать, хотя если его ввести в на сайте то нуэный элемент находит ;(
-        WebElement checkBox = driver.findElement(By.xpath("//tr[@class='row-23 odd']//input"));
-        checkBox.click();
-
-
         //Нужно переключить количество строк в таблице на любое другое значение кроме 10
-        //И нажать чекбокс на первой серой строке с данными "windows	Edge	Samsun	India" чтобы проверка прошла
+        WebElement scrollElement = driver.findElement(By.xpath("//*[@id=\"tablepress-1_length\"]/label/select"));
+        scrollElement.click();
+        scrollElement.findElement(By.xpath("//*[@id=\"tablepress-1_length\"]/label/select/option[2]")).click();
 
+        //И нажать чекбокс на строке с данными "windows	Edge Samsun	India" чтобы проверка прошла
+        WebElement noteElement = driver.findElement(By.xpath("//*[@id=\"tablepress-1\"]/tbody/tr[22]/td[1]/input"));
+        noteElement.click();
+        //Проверка
         assertTrue(driver.findElement(By.xpath("/html/body/div[1]/main/div/div[1]/section[3]/div/div/div/div[3]/div/div/div/table/tbody/tr[22]/td[1]/input")).isSelected());
     }
 
